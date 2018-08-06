7ae16b1e2d6eff7c66e886fd3a2973bc55c5b42c
Tsvetozar Bonev <tsbonev@gmail.com>
2018-08-06 17:12:10 +0300

    Unstage changes

--- mailservice/src/main/kotlin/com/clouway/mailservice/core/Mailer.kt
+++
@@ -1,8 +0,0 @@
-package com.clouway.mailservice.core
-
-/**
- * @author Tsvetozar Bonev (tsbonev@gmail.com)
- */
-interface Mailer {
-    fun mail(receiver: String, title: String, content: String): Int
-}
