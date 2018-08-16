35c8dcb89326021cadd813e499b68cb722bbaafd
Tsvetozar Bonev <tsbonev@gmail.com>
2018-08-16 14:49:02 +0300

    mailservice: add documentation to mailer interface

--- mailservice/src/main/kotlin/com/clouway/mailservice/core/Mailer.kt
+++ mailservice/src/main/kotlin/com/clouway/mailservice/core/Mailer.kt
@@ -1,8 +1,20 @@
 package com.clouway.mailservice.core
 
 /**
+ * Sends an email to a receiver with
+ * a title and content.
+ *
  * @author Tsvetozar Bonev (tsbonev@gmail.com)
  */
 interface Mailer {
+
+    /**
+     * Sends an email.
+     *
+     * @param receiver Receiver of the email
+     * @param title Title of the email
+     * @param content Content of the email
+     * @return A status code
+     */
     fun mail(receiver: String, title: String, content: String): Int
 }
