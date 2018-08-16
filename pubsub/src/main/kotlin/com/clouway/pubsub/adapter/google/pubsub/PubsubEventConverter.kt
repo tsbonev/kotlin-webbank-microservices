47dcbaca4a5832bf76178ca8c8c5d41f6a4ef81e
Tsvetozar Bonev <tsbonev@gmail.com>
2018-08-16 15:07:37 +0300

    pubsub: pass event type and time to register request

--- pubsub/src/main/kotlin/com/clouway/pubsub/adapter/google/pubsub/PubsubEventConverter.kt
+++ pubsub/src/main/kotlin/com/clouway/pubsub/adapter/google/pubsub/PubsubEventConverter.kt
@@ -5,15 +5,20 @@ import com.clouway.pubsub.core.event.Event
 import com.google.appengine.repackaged.com.google.gson.Gson
 import com.google.protobuf.ByteString
 import com.google.pubsub.v1.PubsubMessage
+import java.time.LocalDateTime
 
 /**
  * @author Tsvetozar Bonev (tsbonev@gmail.com)
  */
-internal class PubsubEventConverter : EventConverter<PubsubMessage> {
+internal class PubsubEventConverter(private val getInstant: () -> LocalDateTime = { LocalDateTime.now()})
+    : EventConverter<PubsubMessage> {
     override fun convertEvent(event: Event, eventType: Class<*>): PubsubMessage {
         val gson = Gson()
         val jsonEvent = gson.toJson(event)
         val data = ByteString.copyFromUtf8(jsonEvent)
-        return PubsubMessage.newBuilder().setData(data).putAttributes("eventType", eventType.name).build()
+        return PubsubMessage.newBuilder().setData(data)
+                .putAttributes("eventType", eventType.name)
+                .putAttributes("time", gson.toJson(getInstant()))
+                .build()
     }
 }
